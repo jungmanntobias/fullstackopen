9daf54ee11f7967635cae83ca3a6c647f74e5aa2
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-03 18:08:30 +0300

    initialized phonebook

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,5 @@
 import Course from './components/Course'
 
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = (props) => (
-  <div>
-    {
-      props.parts.map(part_i => <Part key = {part_i.id} part = {part_i}/>)
-    }
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => {
-  const exercise_counts = props.parts.reduce((sum, part_i) => sum + part_i.exercises, 0)
-  
-  return (
-    <p>
-      <b>Number of exercises {exercise_counts}</b>
-    </p>
-  )
-}
-
 const App = () => {
   const courses = [
     {
