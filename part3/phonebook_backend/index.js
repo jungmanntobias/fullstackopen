b4bb605e6a90932d11cb0bd44e8a37cd9ecd0832
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-15 20:16:41 +0300

    3.3 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -37,10 +37,20 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const person = persons.find(p => p.id === request.params.id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
