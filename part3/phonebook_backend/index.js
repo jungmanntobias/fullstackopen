b5458ee85c02f7dd42d7d9cfe52059d083fe2a5f
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-15 21:48:24 +0300

    3.8 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,7 +3,24 @@ var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(function (tokens, req, res) {
+    let tiny =  [
+        tokens.method(req,res),
+        tokens.url(req,res),
+        tokens.status(req,res),
+        tokens.res(req,res, 'content-length'),
+        '-',
+        tokens['response-time'](req,res)
+    ]
+
+    if (req.method === "POST") {
+        tiny = tiny.concat(JSON.stringify(req.body))
+    }
+
+    return tiny.join(' ')
+}))
+
 
 let persons = [
     { 
