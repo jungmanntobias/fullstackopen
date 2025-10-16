e7e9ffcf7e62b7acc718904a93dc868c08b3a3e3
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-16 15:11:03 +0300

    3.18 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -24,6 +24,7 @@ app.use(morgan(function (tokens, req, res) {
     return tiny.join(' ')
 }))
 
+// Retrieving info
 app.get('/info', (request, response, next) => {
     Person.find({})
     .then(persons => {
