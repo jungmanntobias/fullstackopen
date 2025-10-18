afeaca38af8af21e163d5291d520d4bd4c75a0d6
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-18 15:19:52 +0300

    4.2 done

---
+++ part4/blogApp/backend/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blog')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
+logger.info('connecting to', url)
+mongoose
+  .connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/blogApp/backend/index.js
+++ part4/blogApp/backend/index.js
@@ -1,52 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const Blog = require('./models/blog')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-morgan.token('body', req => JSON.stringify(req.body))
-
-//app.use(express.static('dist'))
-app.use(express.json())
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
