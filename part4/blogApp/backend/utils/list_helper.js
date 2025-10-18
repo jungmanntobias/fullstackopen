a109597f886c20cfdec15f22d80f2ce67245ef80
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-18 15:58:35 +0300

    4.5 done

--- part4/blogApp/backend/utils/list_helper.js
+++ part4/blogApp/backend/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, current) => sum + current.likes,0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+    return blogs.reduce((currentBest, current) => {
+        if (current.likes > currentBest.likes) {return current} else {return currentBest}
+    })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
