a3d3499f6ea4303545a13e1e82bc4338587a26d3
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-20 13:28:39 +0300

    5.2 done

--- part5/frontend/src/App.jsx
+++ part5/frontend/src/App.jsx
@@ -19,6 +19,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      //blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -26,6 +35,10 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
     } catch {
       setMessage('wrong credentials')
       setIsError(true)
@@ -71,7 +84,14 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name} logged in 
+        <button type="submit" onClick={() => {
+            window.localStorage.removeItem('loggedBlogappUser')
+            setUser(null)
+          }}>
+          log out
+        </button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
