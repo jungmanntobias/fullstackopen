0b438e14f4bb79c3c5e0b12d98d2e902e2536851
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-21 11:05:59 +0300

    5.14 done

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -25,4 +26,29 @@ test('renders content', () => {
   const likes = screen.getByText('likes: 0')
   expect(url).not.toBeVisible()
   expect(likes).not.toBeVisible()
+})
+
+test('after clicking the button, url and likes are displayed', async () => {
+  const blog = {
+    title: 'test title',
+    author: 'test person',
+    url: 'test url',
+    likes: 0,
+    user: {
+      username: 'testperson',
+      name: 'test person',
+      id: 'test'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view', { exact: false })
+  await user.click(button)
+
+  const url = screen.getByText('url: test url')
+  const likes = screen.getByText('likes: 0')
+  expect(url).toBeVisible()
+  expect(likes).toBeVisible()
 })
