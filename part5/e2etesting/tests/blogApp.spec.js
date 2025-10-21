cdce8e27ddc3a4b7025b10c7c86291e654234303
Tobias Jungmann <tobias.jungmann@aalto.fi>
2025-10-21 20:46:20 +0300

    5.18 done

--- part5/e2etesting/tests/blogApp.spec.js
+++ part5/e2etesting/tests/blogApp.spec.js
@@ -24,7 +24,7 @@ describe('Blog app', () => {
   })
 
   describe('Login', () => {
-    test.only('succeeds with correct credentials', async ({ page }) => {
+    test('succeeds with correct credentials', async ({ page }) => {
       await page.getByLabel('username').fill('testusername')
       await page.getByLabel('password').fill('password')
       await page.getByRole('button').click()
@@ -33,7 +33,12 @@ describe('Blog app', () => {
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      // ...
+      await page.getByLabel('username').fill('testusername')
+      await page.getByLabel('password').fill('wrongpassword')
+      await page.getByRole('button').click()
+
+      await expect(page.getByText('wrong credentials', { exact: false })).toBeVisible()
+      await expect(page.getByText('logged in', { exact: false })).not.toBeVisible()
     })
   })
 
